69687494fd2522133b9624830f6546c42c2a8ba3
farnaztavakool <tavakolfarnaz@gmail.com>
2021-01-13 14:21:05 +1100

    added the solution for question2

--- solution/query.java
+++ solution/query.java
@@ -4,7 +4,13 @@ import java.util.*;
 
 
 public class query {
+   
     
+    /** 
+     * @param s1
+     * @param s2
+     * @return int
+     */
     public int find(String s1,String s2) {
         int count = 0;
         for (int i = 0; i + s2.length() <= s1.length();i++) {
@@ -14,11 +20,23 @@ public class query {
         return count;
     }
 
+    /**
+     * checks if two strings are anagram
+     * @param s1,s2
+     */
+
     public boolean compare(String s1,String s2) {
         return helper(s1).equals(helper(s2));
 
     }
 
+   
+    
+    /** 
+     * create a map of the characters in the string with their quantity
+     * @param s1
+     * @return Map<Character, Integer>
+     */
     public Map <Character,Integer> helper(String s1) {
         HashMap <Character,Integer> s1_char = new HashMap<>();
         for (int i = 0; i < s1.length();i++) {
